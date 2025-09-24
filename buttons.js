0a889e32366ac355bbcf4a4aeac52fb34e5c788c
marcosdavis <macrosdavy@protonmail.com>
2025-09-24 12:17:15 -0600

    added js for nav buttons

---
+++ buttons.js
@@ -0,0 +1,15 @@
+const homeBtn = document.getElementById("home-button")
+const projBtn = document.getElementById("proj-button")
+const resumeBtn = document.getElementById("resume-button")
+
+homeBtn.addEventListener("click", () => {
+    window.location.href = `home.html`
+});
+
+projBtn.addEventListener("click", () => {
+    window.location.href = `projects.html`
+});
+
+resumeBtn.addEventListener("click", () => {
+    window.location.href = `resume.html`
+});
